bf37d365dc66fb1faaefde6558b44de2a1fe1be8
carneirotg <carneirotg@gmail.com>
2024-01-23 21:35:10 +0100

    feat: first commit

---
+++ src/main/java/com/experiment/spotifycustomiser/SpotifycustomiserApplication.java
@@ -0,0 +1,13 @@
+package com.experiment.spotifycustomiser;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpotifycustomiserApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpotifycustomiserApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/experiment/spotifycustomiser/SpotifycustomiserApplicationTests.java
@@ -0,0 +1,13 @@
+package com.experiment.spotifycustomiser;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpotifycustomiserApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
